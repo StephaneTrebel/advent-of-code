533a9e53d5b7caa0eac25360a23a2a8dae9f5cbe
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2026-02-26 20:12:42 +0100

    Optimize 2025/day-09/part-2 💪

--- 2025/day-09/part-2/src/main.rs
+++ 2025/day-09/part-2/src/main.rs
@@ -5,7 +5,7 @@ fn get_file_content(file_path: &str) -> String {
     fs::read_to_string(file_path).expect("Cannot load file")
 }
 
-type Coords = (u64, u64);
+type Coords = (u32, u32);
 
 fn parse_content(content: &str) -> Vec<Coords> {
     content
@@ -13,8 +13,8 @@ fn parse_content(content: &str) -> Vec<Coords> {
         .map(|line| {
             let mut split = line.split(',');
             (
-                split.next().unwrap().parse::<u64>().unwrap(),
-                split.next().unwrap().parse::<u64>().unwrap(),
+                split.next().unwrap().parse::<u32>().unwrap(),
+                split.next().unwrap().parse::<u32>().unwrap(),
             )
         })
         .collect()
@@ -103,7 +103,7 @@ mod tests_get_segments {
     }
 }
 
-fn is_point_on_any_segment(segments: &[(Coords, Coords)], point: &Coords) -> bool {
+fn is_point_on_any_segment(segments: &[(Coords, Coords)], point: Coords) -> bool {
     // Points that are located ON segments
     for ((xa, ya), (xb, yb)) in segments {
         if point.0 == *xa && xa == xb {
@@ -147,7 +147,7 @@ mod tests_is_point_on_any_segment {
 7,3",
         );
         let polygon = get_polygon(&coords_list);
-        assert!(is_point_on_any_segment(&polygon, &(9, 1)));
+        assert!(is_point_on_any_segment(&polygon, (9, 1)));
     }
 
     #[test]
@@ -164,65 +164,121 @@ mod tests_is_point_on_any_segment {
 7,3",
         );
         let polygon = get_polygon(&coords_list);
-        assert!(is_point_on_any_segment(&polygon, &(7, 2)));
+        assert!(is_point_on_any_segment(&polygon, (7, 2)));
     }
 }
 
 fn is_point_inside_polygon(
     raycast_count: &mut RaycastCount,
-    cache: &mut Cache,
     polygon: &[Segment],
-    point: &Coords,
-    (max_x, max_y): &(u64, u64),
+    point: Coords,
+    (_max_x, _max_yy): (u32, u32),
 ) -> bool {
-    if let Some(&b) = cache.get(point) {
-        // println!("CACHED");
-        return b;
-    }
+    // println!("Checking point {point:?}");
     let mut result: bool = false;
 
-    // if polygon.iter().any(|(a, b)| a == point || b == point) {
-    // result = true;
-    // }
+    // Is point a red tile (edge point of the polygon)
+    if polygon.iter().any(|(a, b)| *a == point || *b == point) {
+        result = true;
+    }
+
+    // Is point on any segment that forms the polygon
     if !result && is_point_on_any_segment(polygon, point) {
         result = true;
     }
 
+    // If not, raycast from edge of map following X axis to find how many segments
+    // are in the way.
+    // Even count -> point in outside the polygon
+    // Odd count -> point is inside the polygon
     if !result {
         raycast_count.0 += 1;
-        let mut intersection = 0;
 
-        if point.1 > point.0 {
-            if point.1 > (max_y / 2) {
-                for y in (point.1..*max_y).rev() {
-                    if is_point_on_any_segment(polygon, &(point.0, y)) {
-                        intersection += 1;
+        let edge_point: Coords = (0, point.1);
+        // let edge_point: Coords = {
+        // if point.1 > point.0 {
+        // if point.1 > (max_y / 2) {
+        // (point.0, max_y - 1)
+        // } else {
+        // (point.0, 0)
+        // }
+        // } else if point.0 > (max_x / 2) {
+        // (max_x - 1, point.1)
+        // } else {
+        // (0, point.1)
+        // }
+        // };
+
+        // let x1 = i32::try_from(point.0).unwrap();
+        // let y1 = i32::try_from(point.1).unwrap();
+
+        // let x2 = i32::try_from(edge_point.0).unwrap();
+        // let y2 = i32::try_from(edge_point.1).unwrap();
+
+        // let x3 = i32::try_from(*xs1).unwrap();
+        // let y3 = i32::try_from(*ys1).unwrap();
+
+        // let x4 = i32::try_from(*xs2).unwrap();
+        // let y4 = i32::try_from(*ys2).unwrap();
+
+        // let denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+        // if denominator == 0 {
+        // return false;
+        // }
+        // let t = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4);
+        // let u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3));
+        // let result = denominator >= 0
+        // && (0..=denominator).contains(&t)
+        // && (0..=denominator).contains(&u);
+
+        // println!("EDGE POINT: {edge_point:?}");
+
+        let segment_count = polygon
+            .iter()
+            .filter(|((xs1, ys1), (xs2, ys2))| {
+                let ((xa, ya), (xb, yb)) = {
+                    if ys1 == ys2 {
+                        if xs1 > xs2 {
+                            ((xs2, ys2), (xs1, ys1))
+                        } else {
+                            ((xs1, ys1), (xs2, ys2))
+                        }
+                    } else if ys1 > ys2 {
+                        ((xs2, ys2), (xs1, ys1))
+                    } else {
+                        ((xs1, ys1), (xs2, ys2))
                     }
+                };
+
+                // println!("(xa:{xa},ya:{ya}), (xb:{xb},yb:{yb})");
+
+                // Parallèle
+                if ya == yb && *ya != point.1 {
+                    // println!("Parallèle");
+                    return false;
                 }
-            } else {
-                for y in 0..point.1 {
-                    if is_point_on_any_segment(polygon, &(point.0, y)) {
-                        intersection += 1;
-                    }
+
+                // Intersection
+                if ya != yb && point.1 >= *ya && point.1 <= *yb && *xa <= point.0 {
+                    // println!("Intersection");
+                    return true;
                 }
-            }
-        } else if point.0 > (max_x / 2) {
-            for x in (point.0..*max_x).rev() {
-                if is_point_on_any_segment(polygon, &(x, point.1)) {
-                    intersection += 1;
+
+                // Colinéaire (contrôler qu'au moins un des 2 points est sur notre raycast
+                // horizontal)
+                if ya != yb && point.1 >= *ya && point.1 <= *yb && (*xa <= point.0 || *xb <= point.0)
+                {
+                    // println!("Colinéaire Int.");
+                    return true;
                 }
-            }
-        } else {
-            for x in 0..point.0 {
-                if is_point_on_any_segment(polygon, &(x, point.1)) {
-                    intersection += 1;
-                }
-            }
-        }
-        result = intersection % 2 == 1;
+
+                // println!("False");
+                false
+            })
+            .count();
+        // println!("SEGMENT_COUNT {segment_count}");
+        result = segment_count % 2 == 1;
     }
-
-    cache.insert(*point, result);
 
     result
 }
@@ -247,10 +303,9 @@ mod tests_is_point_inside_polygon {
         let polygon = get_polygon(&coords_list);
         assert!(is_point_inside_polygon(
             &mut RaycastCount(0),
-            &mut HashMap::new(),
             &polygon,
-            &(9, 1),
-            &(12, 12)
+            (9, 1),
+            (12, 12)
         ));
     }
 
@@ -270,10 +325,31 @@ mod tests_is_point_inside_polygon {
         let polygon = get_polygon(&coords_list);
         assert!(is_point_inside_polygon(
             &mut RaycastCount(0),
-            &mut HashMap::new(),
             &polygon,
-            &(9, 2),
-            &(12, 12)
+            (9, 2),
+            (12, 12)
+        ));
+    }
+
+    #[test]
+    fn is_point_inside_polygon_when_it_is_absolutely_not() {
+        let coords_list = parse_content(
+            "\
+7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3",
+        );
+        let polygon = get_polygon(&coords_list);
+        assert!(!is_point_inside_polygon(
+            &mut RaycastCount(0),
+            &polygon,
+            (6, 2),
+            (12, 12)
         ));
     }
 
@@ -293,10 +369,9 @@ mod tests_is_point_inside_polygon {
         let polygon = get_polygon(&coords_list);
         assert!(!is_point_inside_polygon(
             &mut RaycastCount(0),
-            &mut HashMap::new(),
             &polygon,
-            &(0, 0),
-            &(12, 12)
+            (0, 0),
+            (12, 12)
         ));
     }
 
@@ -316,19 +391,18 @@ mod tests_is_point_inside_polygon {
         let polygon = get_polygon(&coords_list);
         assert!(is_point_inside_polygon(
             &mut RaycastCount(0),
-            &mut HashMap::new(),
             &polygon,
-            &(3, 5),
-            &(12, 12)
+            (3, 5),
+            (12, 12)
         ));
     }
 }
 
-fn is_point_inside_rectangle(strict: bool, rectangle: &[Coords; 4], (x, y): &Coords) -> bool {
+fn is_point_inside_rectangle(strict: bool, rectangle: &[Coords; 4], (x, y): Coords) -> bool {
     if strict {
-        *x > rectangle[0].0 && *x < rectangle[1].0 && *y > rectangle[0].1 && *y < rectangle[2].1
+        x > rectangle[0].0 && x < rectangle[1].0 && y > rectangle[0].1 && y < rectangle[2].1
     } else {
-        *x >= rectangle[0].0 && *x <= rectangle[1].0 && *y >= rectangle[0].1 && *y <= rectangle[2].1
+        x >= rectangle[0].0 && x <= rectangle[1].0 && y >= rectangle[0].1 && y <= rectangle[2].1
     }
 }
 
@@ -341,7 +415,7 @@ mod tests_is_point_inside_rectangle {
         assert!(is_point_inside_rectangle(
             false,
             &[(2, 3), (9, 3), (9, 7), (2, 7)],
-            &(9, 4)
+            (9, 4)
         ));
     }
 
@@ -350,7 +424,7 @@ mod tests_is_point_inside_rectangle {
         assert!(!is_point_inside_rectangle(
             true,
             &[(2, 3), (9, 3), (9, 7), (2, 7)],
-            &(9, 4)
+            (9, 4)
         ));
     }
 }
@@ -364,7 +438,7 @@ mod tests_is_point_inside_rectangle {
 ///          ∧         v
 ///          |         |
 ///          D ---<--- C
-fn create_oriented_rectangle(rectangle: &[Coords; 2]) -> [Coords; 4] {
+fn create_oriented_rectangle(rectangle: [Coords; 2]) -> [Coords; 4] {
     let mut x = [rectangle[0].0, rectangle[1].0];
     x.sort_unstable();
     let min_x = x[0];
@@ -388,64 +462,78 @@ type Cache = HashMap<Coords, bool>;
 fn is_valid_rectangle(
     cache: &mut Cache,
     raycast_count: &mut RaycastCount,
-    rectangle: &[Coords; 2],
+    cache_hit_count: &mut CacheHitCount,
+    rectangle: [Coords; 2],
     red_tiles_list: &[Coords],
     polygon: &[Segment],
-    bounding_rect: &(u64, u64),
+    bounding_rect: (u32, u32),
 ) -> bool {
     let candidate_rectangle = create_oriented_rectangle(rectangle);
 
     // println!("\nInput rectangle {rectangle:?}");
     // println!(
-    // "Checking rectangle {candidate_rectangle:?} which has area {}",
-    // compute_rectangle_area(rectangle)
+        // "Checking rectangle {candidate_rectangle:?} which has area {}",
+        // compute_rectangle_area(rectangle)
     // );
-    // println!("red_tiles_list {red_tiles_list:?}");
 
     for red_tile in red_tiles_list {
-        let mut temp_tile: Option<Coords> = None;
-        if is_point_inside_rectangle(true, &candidate_rectangle, red_tile) {
-            temp_tile = Some(*red_tile);
-        }
-        if temp_tile.is_none()
-            && red_tile.0 > candidate_rectangle[0].0
-            && red_tile.0 < candidate_rectangle[2].0
-        {
-            temp_tile = if red_tile.1 < candidate_rectangle[0].1 {
-                Some((red_tile.0, candidate_rectangle[0].1))
-            } else {
-                Some((red_tile.0, candidate_rectangle[2].1))
-            };
-        }
+        let tiles_to_check: Option<Vec<Coords>> = match red_tile {
+            // When the red tile is STRICTLY inside the candidate rectangle
+            tile if is_point_inside_rectangle(true, &candidate_rectangle, *tile) => Some(vec![
+                (tile.0 - 1, tile.1 - 1),
+                (tile.0 - 1, tile.1 + 1),
+                (tile.0 + 1, tile.1 - 1),
+                (tile.0 + 1, tile.1 + 1),
+            ]),
 
-        if temp_tile.is_none()
-            && red_tile.1 > candidate_rectangle[0].1
-            && red_tile.1 < candidate_rectangle[2].1
-        {
-            temp_tile = if red_tile.0 < candidate_rectangle[0].0 {
-                Some((candidate_rectangle[0].0, red_tile.1))
-            } else {
-                Some((candidate_rectangle[2].0, red_tile.1))
-            };
-        }
+            // When the red tile is above or below the candidate rectangle
+            tile if tile.0 > candidate_rectangle[0].0 && tile.0 < candidate_rectangle[2].0 => {
+                if tile.1 < candidate_rectangle[0].1 {
+                    Some(vec![
+                        (tile.0 - 1, candidate_rectangle[0].1 + 1),
+                        (tile.0 + 1, candidate_rectangle[0].1 + 1),
+                    ])
+                } else {
+                    Some(vec![
+                        (tile.0 - 1, candidate_rectangle[2].1 - 1),
+                        (tile.0 + 1, candidate_rectangle[2].1 - 1),
+                    ])
+                }
+            }
 
-        if let Some(tile) = temp_tile {
-            // println!("tile: {:?}", &tile);
-            let cross_points = get_cross_points(&tile);
-            // println!("cross_points: {:?}", &cross_points);
-            for cross_point in cross_points {
-                if is_point_inside_rectangle(false, &candidate_rectangle, &cross_point)
-                    && !is_point_inside_polygon(
-                        raycast_count,
-                        cache,
-                        polygon,
-                        &cross_point,
-                        bounding_rect,
-                    )
-                {
-                    // println!("CROSS POINT IS NOT INSIDE POLYGON {cross_point:?}");
+            // When the red tile is left or right of the candidate rectangle
+            tile if tile.1 > candidate_rectangle[0].1 && tile.1 < candidate_rectangle[2].1 => {
+                if tile.0 < candidate_rectangle[0].0 {
+                    Some(vec![
+                        (candidate_rectangle[0].0 + 1, tile.1 - 1),
+                        (candidate_rectangle[0].0 + 1, tile.1 + 1),
+                    ])
+                } else {
+                    Some(vec![
+                        (candidate_rectangle[2].0 - 1, tile.1 - 1),
+                        (candidate_rectangle[2].0 - 1, tile.1 + 1),
+                    ])
+                }
+            }
+            _ => None,
+        };
+
+        if let Some(tiles) = tiles_to_check {
+            for tile in tiles {
+                // When the tile has already been checked
+                if let Some(&b) = cache.get(&tile) {
+                    // println!("Cache hit ! {tile:?} {b}");
+                    cache_hit_count.0 += 1;
+                    if b {
+                        continue;
+                    }
                     return false;
                 }
+                if !is_point_inside_polygon(raycast_count, polygon, tile, bounding_rect) {
+                    cache.insert(tile, false);
+                    return false;
+                }
+                cache.insert(tile, true);
             }
         }
     }
@@ -453,24 +541,11 @@ fn is_valid_rectangle(
     true
 }
 
-/// For a given X, return a,b,c,d such as :
-///     a b
-///      X
-///     c d
-fn get_cross_points(point: &(u64, u64)) -> [(u64, u64); 4] {
-    [
-        (point.0 - 1, point.1 - 1),
-        (point.0 - 1, point.1 + 1),
-        (point.0 + 1, point.1 - 1),
-        (point.0 + 1, point.1 + 1),
-    ]
-}
-
-fn compute_rectangle_area(coords_list: &[Coords; 2]) -> u64 {
+fn compute_rectangle_area(coords_list: [Coords; 2]) -> u64 {
     let (xa, ya) = coords_list[0];
     let (xb, yb) = coords_list[1];
 
-    (xb.abs_diff(xa) + 1) * (yb.abs_diff(ya) + 1)
+    u64::from((xb.abs_diff(xa) + 1) * (yb.abs_diff(ya) + 1))
 }
 
 #[cfg(test)]
@@ -479,43 +554,44 @@ mod tests_compute_rectangle_area {
 
     #[test]
     fn compute_rectangle_area_01() {
-        assert_eq!(compute_rectangle_area(&[(1, 1), (2, 2)]), 4);
+        assert_eq!(compute_rectangle_area([(1, 1), (2, 2)]), 4);
     }
 
     #[test]
     fn compute_rectangle_area_02() {
-        assert_eq!(compute_rectangle_area(&[(0, 0), (1, 1)]), 4);
+        assert_eq!(compute_rectangle_area([(0, 0), (1, 1)]), 4);
     }
 
     #[test]
     fn compute_rectangle_area_03() {
-        assert_eq!(compute_rectangle_area(&[(1, 1), (0, 0)]), 4);
+        assert_eq!(compute_rectangle_area([(1, 1), (0, 0)]), 4);
     }
 
     #[test]
     fn compute_rectangle_area_04() {
-        assert_eq!(compute_rectangle_area(&[(7, 3), (11, 1)]), 15);
+        assert_eq!(compute_rectangle_area([(7, 3), (11, 1)]), 15);
     }
 
     #[test]
     fn compute_rectangle_area_05() {
-        assert_eq!(compute_rectangle_area(&[(9, 5), (9, 7)]), 3);
+        assert_eq!(compute_rectangle_area([(9, 5), (9, 7)]), 3);
     }
 
     #[test]
     fn compute_rectangle_area_06() {
-        assert_eq!(compute_rectangle_area(&[(2, 3), (9, 5)]), 24);
+        assert_eq!(compute_rectangle_area([(2, 3), (9, 5)]), 24);
     }
 }
 
-struct RaycastCount(u64);
+struct RaycastCount(u32);
+struct CacheHitCount(u32);
 
 #[allow(clippy::too_many_lines)]
 fn fold(red_tiles_list: &[Coords]) -> u64 {
     let mut cache: Cache = HashMap::new();
     let polygon = get_polygon(red_tiles_list);
     // let mut winners = ((0, 0), (0, 0));
-    let mut winners_area = 0;
+    let mut winners_area: u64 = 0;
     let mut count = 0;
     let mut candidate_rectangle_list: Vec<((Coords, Coords), u64)> = vec![];
 
@@ -530,7 +606,7 @@ fn fold(red_tiles_list: &[Coords]) -> u64 {
             if red_tiles_list[i].1 > max_y {
                 max_y = red_tiles_list[i].1;
             }
-            let candidate_area = compute_rectangle_area(&[red_tiles_list[i], red_tiles_list[j]]);
+            let candidate_area = compute_rectangle_area([red_tiles_list[i], red_tiles_list[j]]);
             candidate_rectangle_list.push(((red_tiles_list[i], red_tiles_list[j]), candidate_area));
         }
     }
@@ -538,34 +614,45 @@ fn fold(red_tiles_list: &[Coords]) -> u64 {
     max_x += 1;
     max_y += 1;
 
+    candidate_rectangle_list.sort_unstable_by_key(|c| c.1);
     println!(
         "\nCandidate rectangles: {}\n",
         candidate_rectangle_list.len()
     );
-    candidate_rectangle_list.sort_unstable_by_key(|c| c.1);
 
     let mut raycast_count: RaycastCount = RaycastCount(0);
+    let mut cache_hit_count: CacheHitCount = CacheHitCount(0);
 
     for (candidate_rectangle, area) in candidate_rectangle_list.iter().rev() {
         count += 1;
-        if count % 100 == 0 {
-            println!("{count}");
-        }
-        if is_valid_rectangle(
+        // if count % 100 == 0 {
+        // println!("{count}");
+        // }
+        // if count > 1000 {
+        // break;
+        // }
+        if
+        // *area == 1_529_675_217 &&
+        is_valid_rectangle(
             &mut cache,
             &mut raycast_count,
-            &[candidate_rectangle.0, candidate_rectangle.1],
+            &mut cache_hit_count,
+            [candidate_rectangle.0, candidate_rectangle.1],
             red_tiles_list,
             &polygon,
-            &(max_x, max_y),
+            (max_x, max_y),
         ) {
             winners_area = *area;
             break;
         }
+        // println!("RaycastCount: {}", raycast_count.0);
+        // println!("CacheHitCount: {}", cache_hit_count.0);
+        // println!("Count: {count}\n");
     }
 
-    println!("RaycastCount: {}", raycast_count.0);
-    println!("Count: {count}");
+    // println!("RaycastCount: {}", raycast_count.0);
+    // println!("CacheHitCount: {}", cache_hit_count.0);
+    // println!("Count: {count}");
 
     winners_area
 }
@@ -597,3 +684,9 @@ fn main() {
     let coords_list = parse_content(&file_content);
     println!("Result: {}", fold(&coords_list));
 }
+
+// Result: 1529675217
+//
+// real    3m44,914s
+// user    3m45,002s
+// sys     0m0,126s
